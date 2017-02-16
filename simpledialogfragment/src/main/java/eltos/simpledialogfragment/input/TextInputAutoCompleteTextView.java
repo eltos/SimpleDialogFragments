9f22e9723a32f988931dc53c5b4d5cb19b09a9d4
eltos <eltos@outlook.de>
2017-02-16 12:50:26 +0100

    No hint in IME when in 'extract mode' - FIXED

---
+++ simpledialogfragment/src/main/java/eltos/simpledialogfragment/input/TextInputAutoCompleteTextView.java
@@ -0,0 +1,69 @@
+/*
+ *  Copyright 2017 Philipp Niedermayer (github.com/eltos)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package eltos.simpledialogfragment.input;
+
+import android.content.Context;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.widget.AppCompatAutoCompleteTextView;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewParent;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+
+
+/**
+ * A special sub-class of {@link AppCompatAutoCompleteTextView} designed for use
+ * as a child of {@link TextInputLayout}.
+ *
+ * Using this class allows us to display a hint in the IME when in 'extract' mode.
+ *
+ * Created by eltos on 16.02.17 as suggested here: http://stackoverflow.com/a/41864063
+ *
+ */
+public class TextInputAutoCompleteTextView extends AppCompatAutoCompleteTextView {
+
+    public TextInputAutoCompleteTextView(Context context) {
+        super(context);
+    }
+
+    public TextInputAutoCompleteTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TextInputAutoCompleteTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public InputConnection onCreateInputConnection(EditorInfo outAttrs) {
+        final InputConnection ic = super.onCreateInputConnection(outAttrs);
+        if (ic != null && outAttrs.hintText == null) {
+            // If we don't have a hint and our parent is a TextInputLayout, use it's hint for the
+            // EditorInfo. This allows us to display a hint in 'extract mode'.
+            ViewParent parent = getParent();
+            while (parent instanceof View) {
+                if (parent instanceof TextInputLayout) {
+                    outAttrs.hintText = ((TextInputLayout) parent).getHint();
+                    break;
+                }
+                parent = parent.getParent();
+            }
+        }
+        return ic;
+    }
+}
