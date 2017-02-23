0e810c9604867f780418ce4fa9a780ede9a16db6
eltos <eltos@outlook.de>
2017-02-23 19:22:16 +0100

    New Spinner FormElement for SimpleFormDialog

---
+++ simpledialogfragment/src/main/java/eltos/simpledialogfragment/form/Spinner.java
@@ -0,0 +1,245 @@
+/*
+ *  Copyright 2017 Philipp Niedermayer (github.com/eltos)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package eltos.simpledialogfragment.form;
+
+import android.content.Context;
+import android.os.Parcel;
+import android.support.annotation.ArrayRes;
+import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
+
+
+/**
+ * A spinner form element to be used with {@link SimpleFormDialog}
+ * </p>
+ * This is an Spinner that can be used to give a choice between different values.
+ * </p>
+ * This will add an int to resource bundle containing the index of the chosen value
+ * within the supplied item list or -1 if none was chosen.
+ * </p>
+ * Created by philipp on 23.02.17.
+ */
+
+@SuppressWarnings({"unused", "WeakerAccess"})
+public class Spinner extends FormElement<Spinner, SpinnerViewHolder> {
+
+    private static final int NO_ID = -1;
+
+    private int itemArrayRes = NO_ID;
+    private int[] itemStringResArray = null;
+    private String[] items = null;
+    private String text = null;
+    private int textResourceId = NO_ID;
+    private String placeholder = null;
+    private int placeholderResourceId = NO_ID;
+    int position = -1;
+
+    private Spinner(String resultKey) {
+        super(resultKey);
+    }
+
+
+    /**
+     * Factory method for a plain spinner.
+     *
+     * @param key the key that can be used to receive the chosen item index from the bundle in
+     *            {@link SimpleFormDialog.OnDialogResultListener#onResult}
+     */
+    public static Spinner plain(String key){
+        return new Spinner(key);
+    }
+
+
+
+    /**
+     * Sets the label
+     *
+     * @param text label text as string
+     */
+    public Spinner label(String text){
+        this.text = text;
+        return this;
+    }
+
+    /**
+     * Sets the label
+     *
+     * @param textResourceId label text as android string resource
+     */
+    public Spinner label(@StringRes int textResourceId){
+        this.textResourceId = textResourceId;
+        return this;
+    }
+
+    /**
+     * Sets the placeholder text displayed if nothing is selected
+     *
+     * @param text placeholder text as string
+     */
+    public Spinner placeholder(String text){
+        this.placeholder = text;
+        return this;
+    }
+
+    /**
+     * Sets the placeholder text displayed if nothing is selected
+     *
+     * @param textResourceId placeholder text as android string resource
+     */
+    public Spinner placeholder(@StringRes int textResourceId){
+        this.placeholderResourceId = textResourceId;
+        return this;
+    }
+
+    /**
+     * Provide an array resource with items to be shown by this spinner.
+     *
+     * @param itemArrayRes the string array resource to suggest
+     */
+    public Spinner items(@ArrayRes int itemArrayRes){
+        this.itemArrayRes = itemArrayRes;
+        return this;
+    }
+
+    /**
+     * Provide an array of items to be shown by this spinner.
+     *
+     * @param itemsStringResArray array of string resources to suggest
+     */
+    public Spinner items(@StringRes int... itemsStringResArray){
+        if (itemsStringResArray != null && itemsStringResArray.length > 0) {
+            this.itemStringResArray = itemsStringResArray;
+        }
+        return this;
+    }
+
+    /**
+     * Provide an array of items to be shown by this spinner.
+     *
+     * @param items array of strings to suggest
+     */
+    public Spinner items(String... items){
+        if (items != null && items.length > 0) {
+            this.items = items;
+        }
+        return this;
+    }
+
+    /**
+     * Set the initially selected item
+     *
+     * @param itemIndex The index of the initially selected item
+     */
+    public Spinner preset(int itemIndex){
+        this.position = itemIndex;
+        return this;
+    }
+
+
+
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+
+    @Override
+    public SpinnerViewHolder getViewHolder() {
+        return new SpinnerViewHolder(this);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+
+    @Nullable
+    String getText(Context context){
+        if (text != null) {
+            return text;
+        } else if (textResourceId != NO_ID){
+            return context.getString(textResourceId);
+        }
+        return null;
+    }
+
+    @Nullable
+    String getPlaceholderText(Context context){
+        if (placeholder != null) {
+            return placeholder;
+        } else if (placeholderResourceId != NO_ID){
+            return context.getString(placeholderResourceId);
+        }
+        return null;
+    }
+
+    @Nullable
+    String[] getItems(Context context){
+        if (items != null){
+            return items;
+        } else if (itemStringResArray != null){
+            String[] s = new String[itemStringResArray.length];
+            for (int i = 0; i < itemStringResArray.length; i++) {
+                s[i] = context.getString(itemStringResArray[i]);
+            }
+            return s;
+        } else if (itemArrayRes != NO_ID){
+            return context.getResources().getStringArray(itemArrayRes);
+        }
+        return null;
+    }
+
+
+    // Parcel implementation
+
+    protected Spinner(Parcel in) {
+        super(in);
+        itemArrayRes = in.readInt();
+        itemStringResArray = in.createIntArray();
+        items = in.createStringArray();
+        text = in.readString();
+        textResourceId = in.readInt();
+        placeholder = in.readString();
+        placeholderResourceId = in.readInt();
+        position = in.readInt();
+    }
+
+    public static final Creator<Spinner> CREATOR = new Creator<Spinner>() {
+        @Override
+        public Spinner createFromParcel(Parcel in) {
+            return new Spinner(in);
+        }
+
+        @Override
+        public Spinner[] newArray(int size) {
+            return new Spinner[size];
+        }
+    };
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(itemArrayRes);
+        dest.writeIntArray(itemStringResArray);
+        dest.writeStringArray(items);
+        dest.writeString(text);
+        dest.writeInt(textResourceId);
+        dest.writeString(placeholder);
+        dest.writeInt(placeholderResourceId);
+        dest.writeInt(position);
+    }
+}
---
+++ simpledialogfragment/src/main/java/eltos/simpledialogfragment/form/SpinnerViewHolder.java
@@ -0,0 +1,214 @@
+/*
+ *  Copyright 2017 Philipp Niedermayer (github.com/eltos)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package eltos.simpledialogfragment.form;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import eltos.simpledialogfragment.R;
+
+/**
+ * The ViewHolder class for {@link Spinner}
+ * </p>
+ * This class is used to create a SpinnerView and to maintain it's functionality
+ * </p>
+ * Created by eltos on 23.02.17.
+ */
+
+@SuppressWarnings("WeakerAccess")
+public class SpinnerViewHolder extends FormElementViewHolder<Spinner> {
+
+    public static final int NONE = -1;
+
+    private static final String SAVED_POSITION = "pos";
+    private android.widget.Spinner spinner;
+    private TextView label;
+    private CustomSpinnerAdapter adapter;
+
+    SpinnerViewHolder(Spinner field) {
+        super(field);
+    }
+
+    @Override
+    protected int getContentViewLayout() {
+        return R.layout.dialog_form_item_spinner;
+    }
+
+    @Override
+    protected void setUpView(View view, Context context, Bundle savedInstanceState,
+                             final SimpleFormDialog.DialogActions actions,
+                             final boolean isLastElement, boolean isOnlyElement) {
+
+        spinner = (android.widget.Spinner) view.findViewById(R.id.spinner);
+        label = (TextView) view.findViewById(R.id.label);
+
+        // Label
+        String text = field.getText(context);
+        label.setText(text);
+        label.setVisibility(text == null ? View.GONE : View.VISIBLE);
+
+        // Items
+        String[] items = field.getItems(context);
+        if (items != null) {
+            adapter = new CustomSpinnerAdapter(context, items,
+                    !field.required, field.getPlaceholderText(context));
+            spinner.setAdapter(adapter);
+
+            int preset = field.position >= 0 && field.position < items.length ? field.position : NONE;
+            setSelection(preset);
+        }
+
+        if (savedInstanceState != null) {
+            setSelection(savedInstanceState.getInt(SAVED_POSITION));
+        }
+
+        // Select listener
+//        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                if (!isLastElement) {
+//                    actions.continueWithNextElement();
+//                }
+//            }
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {}
+//        });
+
+
+
+    }
+
+
+
+
+    @Override
+    protected void saveState(Bundle outState) {
+        outState.putInt(SAVED_POSITION, getSelection());
+    }
+
+    @Override
+    protected void putResults(Bundle results, String key) {
+        results.putInt(key, getSelection());
+    }
+
+    @Override
+    protected boolean focus(SimpleFormDialog.FocusActions actions) {
+        actions.hideKeyboard();
+        actions.clearCurrentFocus();
+        spinner.performClick();
+        return spinner.requestFocus();
+    }
+
+    @Override
+    protected boolean posButtonEnabled(Context context) {
+        return !field.required || getSelection() != NONE;
+    }
+
+    @Override
+    protected boolean validate(Context context) {
+        boolean valid = !field.required || getSelection() != NONE;
+        if (valid) {
+            TypedValue value = new TypedValue();
+            label.getContext().getTheme().resolveAttribute(android.R.attr.textColor, value, true);
+//            int[] attr = new int[] {android.R.attr.textColor};
+//            TypedArray a = context.obtainStyledAttributes(value.data, attr);
+            label.setTextColor(value.data);
+//            a.recycle();
+
+        } else {
+            //noinspection deprecation
+            label.setTextColor(context.getResources().getColor(eltos.simpledialogfragment.R.color.error_color));
+        }
+//        checkBox.setError(valid ? null : context.getString(R.string.required));
+        return valid;
+    }
+
+
+
+
+    private int getSelection(){
+        return adapter.mapFromSelection(spinner.getSelectedItemPosition());
+    }
+
+    private void setSelection(int index){
+        spinner.setSelection(adapter.mapToSelection(index), false);
+    }
+
+    private static class CustomSpinnerAdapter extends ArrayAdapter<String> {
+
+        private int emptyIndex;
+        private boolean allowEmpty;
+
+        int mapToSelection(int index){
+            return index == NONE ? emptyIndex : index < emptyIndex ? index : index + 1;
+        }
+        int mapFromSelection(int index){
+            return index == emptyIndex ? NONE : index < emptyIndex ? index : index - 1;
+        }
+
+        CustomSpinnerAdapter(Context context, String[] items, boolean allowEmpty, String emptyString){
+            super(context, android.R.layout.simple_spinner_dropdown_item);
+            this.allowEmpty = allowEmpty;
+            if (allowEmpty) {
+                emptyIndex = 0;
+                add(emptyString);
+                addAll(items);
+            } else {
+                addAll(items);
+                add(emptyString);
+                emptyIndex = items.length;
+            }
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+            View view = super.getView(position, convertView, parent);
+            if (position == emptyIndex) {
+                TextView label = (TextView) view.findViewById(android.R.id.text1);
+                label.setText("");
+                label.setHint(getItem(position));
+            }
+            return view;
+        }
+
+        @Override
+        public View getDropDownView(int position, View convertView, @NonNull ViewGroup parent) {
+            View view =  super.getDropDownView(position, convertView, parent);
+            if (position == emptyIndex) {
+                TextView label = (TextView) view.findViewById(android.R.id.text1);
+                label.setText("");
+                label.setHint(getItem(position));
+            }
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return super.getCount() - (allowEmpty ? 0 : 1);
+        }
+
+    }
+
+
+}
