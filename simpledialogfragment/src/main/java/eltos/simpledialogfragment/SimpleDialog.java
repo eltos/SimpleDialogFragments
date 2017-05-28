b8d2a378b251bafc18b600df6ae7d794f6350e50
eltos <philippjonie@outlook.com>
2017-05-28 12:30:03 +0200

    Optimize imports

--- simpledialogfragment/src/main/java/eltos/simpledialogfragment/SimpleDialog.java
+++ simpledialogfragment/src/main/java/eltos/simpledialogfragment/SimpleDialog.java
@@ -33,7 +33,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.view.ContextThemeWrapper;
 import android.text.Html;
 
 /**
