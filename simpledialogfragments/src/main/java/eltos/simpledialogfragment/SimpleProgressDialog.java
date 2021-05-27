f55c6b94ee25d085d3c6a7f242eaae47eda1fdd8
eltos <eltos@outlook.de>
2021-05-27 11:26:29 +0200

    Add progress percentage and info text

--- simpledialogfragments/src/main/java/eltos/simpledialogfragment/SimpleProgressDialog.java
+++ simpledialogfragments/src/main/java/eltos/simpledialogfragment/SimpleProgressDialog.java
@@ -1,11 +1,15 @@
 package eltos.simpledialogfragment;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import java.text.NumberFormat;
 
 public class SimpleProgressDialog extends CustomViewDialog<SimpleProgressDialog> {
 
@@ -17,6 +21,9 @@ public class SimpleProgressDialog extends CustomViewDialog<SimpleProgressDialog>
             MAX = TAG + "max",
             PROGRESS = TAG + "progress",
             PROGRESS2 = TAG + "progress2",
+            PERCENTAGE = TAG + "percentage",
+            TEXT_PROGRESS = TAG + "text_progress",
+            TEXT_INFO = TAG + "text_info",
             TYPE = TAG + "type";
 
     /**
@@ -24,17 +31,33 @@ public class SimpleProgressDialog extends CustomViewDialog<SimpleProgressDialog>
      * Can be set via {@link SimpleProgressDialog#type(Type)}
      * Please note that some features might not be available depending on the chosen type
      */
-    public enum Type {BAR, CIRCLE};
+    public enum Type {
+        /**
+         * A horizontal progress bar
+         * {@link R.layout#simpledialogfragment_progress}
+         */
+        BAR,
+
+        /**
+         * A circle (only indeterminate)
+         * {@link R.layout#simpledialogfragment_progress_circle}
+         */
+        CIRCLE,
+    };
 
 
     public static SimpleProgressDialog build(){
         return new SimpleProgressDialog();
     }
     public static SimpleProgressDialog bar(){
-        return SimpleProgressDialog.build().type(Type.BAR);
+        return SimpleProgressDialog.build()
+                .type(Type.BAR)
+                .percentage(true);
     }
     public static SimpleProgressDialog indeterminateCircle(){
-        return SimpleProgressDialog.build().type(Type.CIRCLE);
+        return SimpleProgressDialog.build()
+                .type(Type.CIRCLE)
+                .percentage(false);
     }
 
     public SimpleProgressDialog(){
@@ -54,6 +77,16 @@ public class SimpleProgressDialog extends CustomViewDialog<SimpleProgressDialog>
      */
     public SimpleProgressDialog type(Type type){
         return setArg(TYPE, type.ordinal());
+    }
+
+    /**
+     * Whether to show the percentage text or not
+     *
+     * @param visible percentage text visible or not
+     * @return this instance
+     */
+    public SimpleProgressDialog percentage(boolean visible){
+        return setArg(PERCENTAGE, visible);
     }
 
 
@@ -79,6 +112,17 @@ public class SimpleProgressDialog extends CustomViewDialog<SimpleProgressDialog>
             if (progress != null) mProgressBar.setProgress(progress);
             if (secondaryProgress != null) mProgressBar.setSecondaryProgress(secondaryProgress);
             if (max != null) mProgressBar.setMax(max);
+
+            // set percentage text if not disabled
+            if (getArgs().getBoolean(PERCENTAGE)){
+                double percent;
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                    percent = 1.0 * (mProgressBar.getProgress() - mProgressBar.getMin()) / (mProgressBar.getMax() -mProgressBar.getMin());
+                } else {
+                    percent = 1.0 * mProgressBar.getProgress() / mProgressBar.getMax();
+                }
+                updateProgressTextInternal(NumberFormat.getPercentInstance().format(percent));
+            }
         }
 
     }
@@ -126,12 +170,46 @@ public class SimpleProgressDialog extends CustomViewDialog<SimpleProgressDialog>
         updateProgress(true, null, null, null);
     }
 
+    /**
+     * Set or update the progress text at the start of the progress bar /
+     * in the center of the circle
+     *
+     * @param text The text to show
+     */
+    public void updateProgressText(String text){
+        percentage(false);
+        updateProgressTextInternal(text);
+    }
+
+    private void updateProgressTextInternal(String text){
+        if (mProgressText != null) {
+            mProgressText.setText(text);
+            mProgressText.setVisibility(TextUtils.isEmpty(text) ? View.GONE : View.VISIBLE);
+        } else { // not yet shown, update arguments instead
+            setArg(TEXT_PROGRESS, text);
+        }
+    }
+
+    /**
+     * Set or update the info text at the end of the bar / next to the circle
+     *
+     * @param text The text to show
+     */
+    public void updateInfoText(String text){
+        if (mInfoText != null) {
+            mInfoText.setText(text);
+            mInfoText.setVisibility(TextUtils.isEmpty(text) ? View.GONE : View.VISIBLE);
+        } else { // not yet shown, update arguments instead
+            setArg(TEXT_INFO, text);
+        }
+    }
 
 
 
 
-
-    public ProgressBar mProgressBar;
+    protected ProgressBar mProgressBar;
+    protected TextView mProgressText;
+    protected TextView mInfoText;
 
 
 
@@ -140,19 +218,23 @@ public class SimpleProgressDialog extends CustomViewDialog<SimpleProgressDialog>
     protected View onCreateContentView(Bundle savedInstanceState) {
         // inflate and set your custom view here
 
-        View view = inflate(R.layout.simpledialogfragment_progress);
+        View view;
         if (getArgs().getInt(TYPE) == Type.CIRCLE.ordinal()) {
-            mProgressBar = view.findViewById(R.id.progressCircle);
+            view = inflate(R.layout.simpledialogfragment_progress_circle);
         } else { // default: Type.BAR
-            mProgressBar = view.findViewById(R.id.progress);
+            view = inflate(R.layout.simpledialogfragment_progress);
         }
+        mProgressBar = view.findViewById(R.id.progress);
+        mProgressText = view.findViewById(R.id.progressText);
+        mInfoText = view.findViewById(R.id.infoText);
 
 
         if (savedInstanceState == null){
             savedInstanceState = getArgs();
         }
 
-        mProgressBar.setVisibility(View.VISIBLE);
+        updateInfoText(savedInstanceState.getString(TEXT_INFO));
+        updateProgressTextInternal(savedInstanceState.getString(TEXT_PROGRESS));
         updateProgress(savedInstanceState.getBoolean(INDETERMINATE, true),
                        savedInstanceState.getInt(PROGRESS, 0),
                        savedInstanceState.getInt(PROGRESS2, 0),
@@ -168,6 +250,8 @@ public class SimpleProgressDialog extends CustomViewDialog<SimpleProgressDialog>
         outState.putInt(MAX, mProgressBar.getMax());
         outState.putInt(PROGRESS, mProgressBar.getProgress());
         outState.putInt(PROGRESS2, mProgressBar.getSecondaryProgress());
+        outState.putString(TEXT_INFO, String.valueOf(mInfoText.getText()));
+        outState.putString(TEXT_PROGRESS, String.valueOf(mProgressText.getText()));
     }
 
 }
